ce7cca3a4e4c97719cd6b727e20b17ccdeb41519
Dramoun <j.holub99@gmail.com>
2020-12-09 22:51:44 +0100

    Field class finished? Game logic class created and game loop implemented. To be completed

--- testEnv.py
+++ testEnv.py
@@ -30,6 +30,15 @@ class PlayField:
 
         self.field = self.tempField
 
+    def initialUpdate(self):
+        self.endBoolFound = False
+
+        self.updateNodeValue(self.getPosByStateValue("state", "start"), "g", 0)
+        self.updateNodeValue(self.getPosByStateValue("state", "start"), "parent",
+                             self.getPosByStateValue("state", "start"))
+        self.updateNodeValue(self.getPosByStateValue("state", "end"), "h", 0)
+        self.updateAround(self.getPosByStateValue("state", "start"))
+
     def genRandMap(self):
         walls = self.wallCount
 
@@ -80,6 +89,49 @@ class PlayField:
             return False
 
         return True
+
+    def updateAround(self, position):
+        aList = self.getAround(position)
+
+        for ite, cords in enumerate(aList):
+
+            if cords is not None:
+
+                if self.field[cords]["state"] in ("open", "closed", "None"):
+
+                    if ite < 4:
+                        disValue = 14
+
+                    else:
+                        disValue = 10
+
+                    oriG = self.getNodeItem(position, "g")
+
+                    nodeG = oriG + disValue
+                    nodeH = self.calcNodeNum(cords, self.getPosByStateValue("state", "end"))
+                    nodeF = nodeG + nodeH
+
+                    if nodeF < self.getNodeItem(cords, "f"):
+                        self.updateNodeValue(cords, "g", nodeG)
+                        self.updateNodeValue(cords, "h", nodeH)
+                        self.updateNodeValue(cords, "f", nodeF)
+                        self.updateNodeValue(cords, "parent", position)
+
+                    if self.field[cords]["state"] == "None":
+                        self.updateNodeValue(cords, "state", "open")
+
+                elif self.field[cords]["state"] == "end":
+                    self.endBoolFound = True
+                    self.updateNodeValue(cords, "parent", position)
+
+                elif self.field[cords]["state"] == "unseenWall":
+                    self.field[cords]["state"] = "seenWall"
+
+        if self.getNodeItem(position, "state") == "open":
+            self.updateNodeValue(position, "state", "closed")
+
+    def updateNodeValue(self, position, key, value):
+        self.field[position][key] = value
 
     def getAround(self, position):
         sideMin = 0
@@ -138,6 +190,26 @@ class PlayField:
 
         return tl, tr, bl, br, tm, left, right, bm
 
+    def getPosByStateValue(self, item, value):
+        for pos, items in self.field.items():
+            if items != {} and items[item] == value:
+                return pos
+
+    def getNodeItem(self, pos, item):
+        return self.field[pos][item]
+
+    def getLowestFNode(self):
+        tempF = math.inf
+        nodePos = tuple
+
+        for pos, items in self.field.items():
+            if items["state"] == "open":
+                if items["f"] < tempF:
+                    tempF = items["f"]
+                    nodePos = pos
+
+        return nodePos
+
     @staticmethod
     def genStartEnd():
         posChoices = (0, 1, 2, 7, 8, 9)
@@ -176,66 +248,6 @@ class PlayField:
 
         return (startX, startY), (endX, endY)
 
-    def updateAround(self, position):
-        aList = self.getAround(position)
-
-        for ite, cords in enumerate(aList):
-
-            if cords is not None:
-
-                if self.field[cords]["state"] in ("open", "closed", "None"):
-
-                    if ite < 4:
-                        disValue = 14
-
-                    else:
-                        disValue = 10
-
-                    oriG = self.getNodeItem(position, "g")
-
-                    nodeG = oriG + disValue
-                    nodeH = self.calcNodeNum(cords, self.getPosByStateValue("state", "end"))
-                    nodeF = nodeG + nodeH
-
-                    if nodeF < self.getNodeItem(cords, "f"):
-                        self.updateNodeValue(cords, "g", nodeG)
-                        self.updateNodeValue(cords, "h", nodeH)
-                        self.updateNodeValue(cords, "f", nodeF)
-                        self.updateNodeValue(cords, "parent", position)
-
-                    if self.field[cords]["state"] == "None":
-                        self.updateNodeValue(cords, "state", "open")
-
-                elif self.field[cords]["state"] == "end":
-                    self.endBoolFound = True
-                    self.updateNodeValue(cords, "parent", position)
-
-                elif self.field[cords]["state"] == "unseenWall":
-                    self.field[cords]["state"] = "seenWall"
-
-        if self.getNodeItem(position, "state") == "open":
-            self.updateNodeValue(position, "state", "closed")
-
-    def initialUpdate(self):
-        self.endBoolFound = False
-
-        self.updateNodeValue(self.getPosByStateValue("state", "start"), "g", 0)
-        self.updateNodeValue(self.getPosByStateValue("state", "start"), "parent",
-                             self.getPosByStateValue("state", "start"))
-        self.updateNodeValue(self.getPosByStateValue("state", "end"), "h", 0)
-        self.updateAround(self.getPosByStateValue("state", "start"))
-
-    def updateNodeValue(self, position, key, value):
-        self.field[position][key] = value
-
-    def getPosByStateValue(self, item, value):
-        for pos, items in self.field.items():
-            if items != {} and items[item] == value:
-                return pos
-
-    def getNodeItem(self, pos, item):
-        return self.field[pos][item]
-
     @staticmethod
     def calcNodeNum(pos, endPos):
         dX = abs(pos[0] - endPos[0])
@@ -246,57 +258,64 @@ class PlayField:
 
         return int(14 * dX + (10 * (dY - dX)))
 
-    def getLowestFNode(self):
-        tempF = math.inf
-        nodePos = tuple
 
+class GameLogic:
+
+    def __init__(self, sqSize, fieldSize):
+        self.sqSize = sqSize
+        self.fieldSize = fieldSize
+
+        self.nodeBoarderSize = 1
+        self.field = PlayField(fieldSize, 40, 15, 30).field
+        self.gameActiveBool = True
+        self.gameName = 'visualApath'
+        self.gameRes = (sqSize*fieldSize, sqSize*fieldSize)
+
+        self.gameScreen = pygame.display.set_mode(self.gameRes)
+
+    def initialLoad(self):
+        pygame.init()
+        pygame.display.set_caption(self.gameName)
+
+    def mainLoop(self):
+        self.initialLoad()
+
+        while self.gameActiveBool:
+
+            self.drawField()
+
+            for ev in pygame.event.get():
+
+                if ev.type == pygame.QUIT:
+
+                    self.gameActiveBool = False
+                    pygame.quit()
+
+            pygame.display.update()
+
+    def drawField(self):
         for pos, items in self.field.items():
-            if items["state"] == "open":
-                if items["f"] < tempF:
-                    tempF = items["f"]
-                    nodePos = pos
+            pygame.draw.rect(self.gameScreen, self.getColorByState(items["state"]),
+                             ((pos[0] * self.sqSize) + self.nodeBoarderSize, ((pos[1] * self.sqSize) + self.nodeBoarderSize), 62, 62))
 
-        return nodePos
-
-
-field = PlayField(10, 40, 15, 30)
-
-gameActiveBool = True
-res = (640, 640)
-screen = pygame.display.set_mode(res)
-pygame.init()
-pygame.display.set_caption('visualApath')
-
-
-def getColorByState(state):
-    if state == "start":
-        return 25, 121, 169
-    elif state == "end":
-        return 68, 188, 216
-    elif state == "seenWall":
-        return 128, 57, 30
-    elif state == "open":
-        return 237, 184, 121
-    elif state == "closed":
-        return 224, 123, 57
-    elif state == "None":
-        return 255, 255, 255
-    elif state == "path":
-        return 0, 255, 0
-    elif state == "unseenWall":
-        return 128, 57, 30
+    def getColorByState(self, state):
+        if state == "start":
+            return 25, 121, 169
+        elif state == "end":
+            return 68, 188, 216
+        elif state == "seenWall":
+            return 128, 57, 30
+        elif state == "open":
+            return 237, 184, 121
+        elif state == "closed":
+            return 224, 123, 57
+        elif state == "None":
+            return 255, 255, 255
+        elif state == "path":
+            return 0, 255, 0
+        elif state == "unseenWall":
+            return 128, 57, 30
 
 
-while gameActiveBool:
-
-    for pos, items in field.field.items():
-        pygame.draw.rect(screen, getColorByState(items["state"]),
-                         ((pos[0] * 64) + 1, ((pos[1] * 64) + 1), 62, 62))
-
-    for ev in pygame.event.get():
-        if ev.type == pygame.QUIT:
-            gameActiveBool = False
-
-    pygame.display.update()
-
-pygame.quit()
+run = GameLogic(64, 10)
+run.mainLoop()
